81971961825b159396619859ec28d93f2f99a5f9
Vlad Sharaev <sharaev.vl.vl@gmail.com>
2024-09-07 12:34:40 +0300

    Add the project

---
+++ PreparationFlow/PreparationFlow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PreparationFlow
+//
+//  Created by Macbook Gray on 9/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
